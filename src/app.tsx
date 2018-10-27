41696039a84f5b7a1ffd9c1f5132b20200f62698
Lachlan van der Velden <me@lachlanvelden.com>
2018-10-27 18:40:19 +1100

    Inital Commit

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { HomePage } from 'pages/index';
+
+export default class App extends React.Component<{}> {
+    render() {
+        return <HomePage />;
+    }
+}
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class HomePage extends React.Component<{}> {
+    render() {
+        return (
+            <div>
+                Home page
+            </div>
+        );
+    }
+}
